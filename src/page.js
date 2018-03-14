e428e5c46e3657a67cfdbd6703f1a5671774e4bc
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-03-13 22:02:29 -0300

    esboço da tela

--- src/page.js
+++ src/page.js
@@ -1,5 +1,27 @@
 import React from 'react'
 
 export default props => (
-  <h1>Exercises for you upgrade your english skills</h1>
+  <div>
+    <h1 style={{ textAlign: 'center' }}>Exercises for you upgrade your english skills</h1>
+    <label style={{
+      textAlign: 'center',
+      color: 'green',
+      marginTop: '50px',
+      fontSize: '6rem',
+      display: 'block',
+      textAlign: 'center'
+    }}>
+      Car
+    </label>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <input type='text' style={{
+        fontSize: '2rem',
+        textAlign: 'center',
+        height: '30px',
+        marginTop: '2rem',
+        borderRadius: '10px',
+        padding: '8px'
+      }}/>
+    </div>
+  </div>
 )
