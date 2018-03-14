917ea3a04e962267d7723d672c1ae28d2cd460fb
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-03-13 21:08:06 -0300

    setup configuration

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Page from './page'
+
+render(
+  <Page />,
+  document.getElementById('app')
+)
---
+++ src/page.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default props => (
+  <h1>Exercises for you upgrade your english skills</h1>
+)
